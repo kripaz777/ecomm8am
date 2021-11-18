70dcfecec8d7c56481b53836bbe3e6de50aaaabb
kripaz777 <aceraayush@gmail.com>
2021-11-18 09:30:14 +0545

    Template intigrations and Wrked on views

--- home/views.py
+++ home/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import View
 
+from .models import *
 # Create your views here.
+class BaseView(View):
+	views = {}
+	views['category'] = Category.objects.all()
+	views['subcategoty'] = SubCategory.objects.all()
+
+class HomeView(BaseView):
+	def get(self,request):
+		# self.views['items'] = Item.objects.all()
+		self.views['items'] = Item.objects.filter(stock = 'In Stock')
+		self.views['ads'] = Ad.objects.all()
+		self.views['sliders'] = Slider.objects.all()
+
+		return render(request,'index.html',self.views)
